877d649332159fa33bd4b36c39a849b8381e574a
Rafly Maulana <mraflymaulana@gmail.com>
2021-08-04 15:28:42 +0700

    Fix missing typings

---
+++ src/untyped.d.ts
@@ -0,0 +1,8 @@
+/**
+ * This files are used to fix missing typings for several depedencies.
+ */
+
+declare module "figlet/importable-fonts/Doom" {
+    const Doom: string;
+    export default Doom;
+}
