1c5c402b435cdb8bf924a59bbf60b2b7531c3a8c
Rafly Maulana <mraflymaulana@gmail.com>
2021-08-13 14:19:08 +0700

    remove constant variable, now using only window extended variable

--- src/ui/nui/modules/WINDOW_EVENT_EMITTER.js
+++ src/ui/nui/modules/WINDOW_EVENT_EMITTER.js
@@ -2,22 +2,22 @@
  * @description
  * List of events registered
  */
-const events = (window.events = []);
+window.events = [];
 
 /**
  * @description
  * Trigger an event to another NUI
  */
-const emit = (window.emit = (name, data = {}) => {
+window.emit = (name, data = {}) => {
     const event = new CustomEvent(name, { detail: data });
     window.dispatchEvent(event);
-});
+};
 
 /**
  * @description
  * Listen for event
  */
-const on = (window.on = (name, handler) => {
+window.on = (name, handler) => {
     const index = window.events.length;
     const wrapper = (event) => handler(event.detail);
 
@@ -25,25 +25,25 @@ const on = (window.on = (name, handler) => {
     window.events.push({ index, wrapper, name });
 
     return index;
-});
+};
 
 /**
  * @description
  * Remove an event listener
  */
-const removeListener = (window.removeListener = (name, index) => {
+window.removeListener = (name, index) => {
     for (const listener of window.events) {
         if (listener.name === name && (!listener.index || listener.index === index)) {
             window.removeEventListener(name, listener.wrapper);
         }
     }
-});
+};
 
 /**
  * @description
  * Send data back to client (use addNUIEventhandler on the client script)
  */
-const sendData = (window.sendData = (name, data = {}) => {
+window.sendData = (name, data = {}) => {
     name = encodeURIComponent(name);
     $.post(`https://${GetParentResourceName()}/${name}`, JSON.stringify(data));
-});
+};
