6ee3b23d34d8ec5d65c00de80a063896f93b0f3c
Rafly Maulana <mraflymaulana@gmail.com>
2021-08-23 20:03:37 +0700

    Add prettier configuration file

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    trailingComma: "es5",
+    bracketSpacing: true,
+    arrowParens: "always",
+    endOfLine: "auto",
+    htmlWhitespaceSensitivity: "css",
+    jsxBracketSameLine: true,
+    jsxSingleQuote: true,
+    printWidth: 200,
+    semi: true,
+    tabWidth: 4,
+};
