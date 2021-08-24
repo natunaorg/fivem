53dd14e3f1a47f67f7120b1ba7391795dc87b7db
Rafly Maulana <mraflymaulana@gmail.com>
2021-08-24 12:01:07 +0700

    Remove figlet modules, unused

--- src/untyped.d.ts
+++ src/untyped.d.ts
@@ -1,8 +1,3 @@
 /**
  * This files are used to fix missing typings for several depedencies.
  */
-
-declare module "figlet/importable-fonts/Doom" {
-    const Doom: string;
-    export default Doom;
-}
