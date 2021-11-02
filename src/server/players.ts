34b183aedf4c3b5eced6fd7546ffad27fe6f7b32
Rafly Maulana <mraflymaulana@gmail.com>
2021-11-03 00:47:42 +0700

    Update players.ts

--- src/server/players.ts
+++ src/server/players.ts
@@ -286,7 +286,7 @@ export default class Players {
                 ip?: string;
                 [key: string]: any;
             } = {
-                license: fxdkMode ? "fxdk_license" : undefined
+                license: fxdkMode ? "fxdk_license" : undefined,
             };
 
             for (let i = 0; i < GetNumPlayerIdentifiers(String(playerServerId)); i++) {
