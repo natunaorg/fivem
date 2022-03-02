349c571cb86aadf53715f5c37825909ca52fd339
Rafly Maulana <mraflymaulana@gmail.com>
2022-03-03 01:17:28 +0700

    create a base event wrapper
    used to initializing all event-related module

---
+++ src/shared/events/base.ts
@@ -0,0 +1,44 @@
+"use strict";
+import "@citizenfx/client";
+
+type Listeners = {
+    id: number;
+    name: string;
+    handler: (...args: any) => any;
+};
+
+export type CallbackValueData = {
+    uniqueId: string;
+    values: any;
+};
+
+export type EmitData = {
+    name: string;
+    uniqueId: string;
+    args: any[];
+};
+
+export default class EventBase {
+    protected listenerCounter = 1;
+    protected listeners: Listeners[] = [];
+    protected callbackValues: CallbackValueData[] = [];
+
+    protected __validateEventName = (name: string | string[]) => {
+        // Check if the name is an array or string
+        if ((typeof name == "object" && Array.isArray(name)) || typeof name !== "string") {
+            throw new Error(`Invalid Event Name Properties for ${name}`);
+        }
+
+        // If the name was a string then convert it to an array
+        if (typeof name === "string") {
+            name = [name];
+        }
+
+        // Check if the name is an array of strings
+        if (name.find((alias) => typeof alias !== "string")) {
+            throw new Error(`Invalid Event Name Properties for ${name}`);
+        }
+
+        return name;
+    };
+}
