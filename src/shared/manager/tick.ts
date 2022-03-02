6c077d165ed4d61087bbafd3a6091b88a94673ec
Rafly Maulana <mraflymaulana@gmail.com>
2022-03-03 01:15:22 +0700

    Add a tick manager
    all ticks should be run under one module so all memory management would go into single resource to reduce memory usage and etc
    
    in the future, i might create a workaround to optimizating all memory usage on this manager

---
+++ src/shared/manager/tick.ts
@@ -0,0 +1,39 @@
+type TickHandler = () => void;
+
+export default class TickManager {
+    constructor() {
+        setTick(() => {
+            for (const tick of this.#list) {
+                tick.handler();
+            }
+        });
+    }
+
+    #count = 0;
+    #list: { id: number; handler: TickHandler }[] = [];
+
+    set = (handler: TickHandler) => {
+        const id = this.#count;
+
+        this.#list.push({ id, handler });
+        this.#count++;
+
+        return id;
+    };
+
+    remove = (id: number) => {
+        this.#list = this.#list.filter((t) => t.id !== id);
+        return true;
+    };
+
+    update = (id: number, handler: TickHandler) => {
+        const index = this.#list.findIndex((t) => t.id === id);
+
+        if (index === -1) {
+            return false;
+        }
+
+        this.#list[index].handler = handler;
+        return true;
+    };
+}
