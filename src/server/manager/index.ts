bf8a4bfe7bb81e978f0874b99992e66a73f52a4e
Rafly Maulana <mraflymaulana@gmail.com>
2022-03-03 01:21:32 +0700

    create manager module for server

---
+++ src/server/manager/index.ts
@@ -0,0 +1,16 @@
+"use strict";
+import "@citizenfx/server";
+
+import type Events from "@server/events";
+import type Players from "@server/players";
+
+import CommandManager from "@server/manager/command";
+import TickManager from "@shared/manager/tick";
+import Utils from "@server/utils";
+
+export default class Manager {
+    constructor(private events: Events, private players: Players, private utils: Utils) {}
+
+    tick = new TickManager();
+    command = new CommandManager(this.events, this.players, this.utils);
+}
