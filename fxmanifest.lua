bb87dd4faec99482396820280e58a6d68ad53bd2
Rafly Maulana <mraflymaulana@gmail.com>
2022-03-03 01:54:21 +0700

    update metadata and funding

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game {'gta5'}
 
 author 'Rafly Maulana'
 description 'FiveM Typescript/Javascript Bundled Framework with single module engine that runs on Javascript runtime. Powered with NodeJS.'
-version '1.4.0'
+version '1.5.0'
 
 ui_page 'src/ui/nui/nui.html'
 loadscreen 'src/ui/loading_screen/index.html'
