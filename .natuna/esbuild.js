1f0d25f763582edb886eed2357f6a452e48f151c
Rafly Maulana <mraflymaulana@gmail.com>
2022-03-07 21:00:32 +0700

    Add loader plugin
    assign an absolute path to all node_modules so all plugins load from local project instead of fivem yarn cache

--- .natuna/esbuild.js
+++ .natuna/esbuild.js
@@ -1,30 +1,65 @@
 "use strict";
 const esbuild = require("esbuild");
+const path = require("path");
 
 const isProduction = process.argv.findIndex((argItem) => argItem === "--mode=production") >= 0;
 const isWatch = process.argv.findIndex((argItem) => argItem === "--watch") >= 0;
 
 (async () => {
     /**
-     * @type {(import("esbuild").BuildOptions & {
-     *  label: string
-     * })[]}
+     * @type {(import("esbuild").BuildOptions & { label: string })[]}
      */
     const contexts = [
-        {
-            label: "server",
-            platform: "node",
-            entryPoints: ["./src/server/index.ts"],
-            target: ["node16"],
-            external: ["./node_modules/*", "@/*"],
-            format: "cjs",
-        },
         {
             label: "client",
             platform: "browser",
             entryPoints: ["./src/client/index.ts"],
             target: ["chrome93"],
             format: "iife",
+        },
+        {
+            label: "server",
+            platform: "node",
+            entryPoints: ["./src/server/index.ts"],
+            target: ["node16"],
+            external: [],
+            inject: [],
+            format: "cjs",
+            sourcemap: true,
+            define: {
+                require: "requireTo",
+            },
+            plugins: [
+                {
+                    name: "ts-paths",
+                    setup: (build) => {
+                        build.onResolve({ filter: /@citizenfx/ }, (args) => {
+                            return { namespace: "ignore", path: "." };
+                        });
+
+                        build.onResolve({ filter: /.*/ }, (args) => {
+                            if (!args.path.match(/^@(server|client|shared|\/)/) && args.kind === "import-statement") {
+                                let modulePath = require.resolve(args.path);
+
+                                if (path.isAbsolute(modulePath)) {
+                                    modulePath = path.join(...process.cwd().split(path.sep), "node_modules", args.path);
+                                }
+
+                                return {
+                                    path: modulePath,
+                                    external: true,
+                                };
+                            }
+                        });
+
+                        build.onLoad({ filter: /.*/, namespace: "ignore" }, (args) => {
+                            return {
+                                contents: "",
+                            };
+                        });
+                    },
+                },
+            ],
         },
     ];
 
@@ -55,12 +90,14 @@ const isWatch = process.argv.findIndex((argItem) => argItem === "--watch") >= 0;
                 ...context,
             });
 
-            const analize = await esbuild.analyzeMetafile(result.metafile, {
-                color: true,
-                verbose: true,
-            });
+            if (isProduction) {
+                const analize = await esbuild.analyzeMetafile(result.metafile, {
+                    color: true,
+                    verbose: true,
+                });
 
-            console.log(analize);
+                console.log(analize);
+            }
         } catch (error) {
             console.error(`[${label}]: Build failed`, error);
         }
