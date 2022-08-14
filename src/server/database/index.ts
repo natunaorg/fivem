bbec3ba87b0c07bf497597091e5f9347af75a970
Rafly Maulana <mraflymaulana@gmail.com>
2022-08-15 00:17:50 +0700

    Add password into database .env config

--- src/server/database/index.ts
+++ src/server/database/index.ts
@@ -27,6 +27,7 @@ export default function Database(logger: Logger) {
                 host: process.env.DATABASE_HOST,
                 user: process.env.DATABASE_USER,
                 database: process.env.DATABASE_NAME,
+                password: process.env.DATABASE_PASSWORD,
                 port: Number(process.env.DATABASE_PORT),
                 waitForConnections: true,
                 connectionLimit: 10,
